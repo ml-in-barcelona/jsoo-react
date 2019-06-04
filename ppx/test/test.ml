bc97a74ef46677ffb788b94c2dafecce8b07e0f8
Javier Chávarri <javier.chavarri@gmail.com>
2019-06-04 20:09:23 +0200

    Add more ppx tests

--- ppx/test/test.ml
+++ ppx/test/test.ml
@@ -1,12 +1,40 @@
-let make ?(name = "") =
-  ([ (div ~children:[React.string ("Hello " ^ name)] () [@JSX])
-  ; (Hello.createElement ~one:"1" ~children:[React.string ("Hello " ^ name)] () 
-    [@JSX]) ] [@JSX])
-  [@@react.component]
-
-module ForwardRef = struct
-  let make =
-    React.forwardRef (fun theRef ->
-        (div ~ref:theRef ~children:["ForwardRef" |. React.string] () [@JSX]) )
-    [@@react.component]
-end
+let make ?(name= "")  =
+  (([((div ~children:[React.string ("Hello " ^ name)] ())
+    [@JSX ]);
+    ((Hello.createElement ~one:"1" ~children:[React.string ("Hello " ^ name)]
+        ())
+    [@JSX ])])
+  [@JSX ])[@@react.component ]
+let make ~a  ~b  _ =
+  Js.log "This function should be named `Test`";
+  ((div ~children:[] ())
+  [@JSX ])[@@react.component ]
+module Bar =
+  struct
+    let make ~a  ~b  _ =
+      Js.log "This function should be named `Test$Bar`";
+      ((div ~children:[] ())
+      [@JSX ])[@@react.component ]
+    let component ~a  ~b  _ =
+      Js.log "This function should be named `Test$Bar$component`";
+      ((div ~children:[] ())
+      [@JSX ])[@@react.component ]
+  end
+module type X_int  = sig val x : int end
+module Func(M:X_int) =
+  struct
+    let x = M.x + 1
+    let make ~a  ~b  _ =
+      Js.log "This function should be named `Test$Func`" M.x;
+      ((div ~children:[] ())
+      [@JSX ])[@@react.component ]
+  end
+module ForwardRef =
+  struct
+    let make =
+      React.forwardRef
+        (fun theRef ->
+           ((div ~ref:theRef ~children:["ForwardRef" |. React.string] ())
+           [@JSX ]))[@@react.component ]
+  end
+let fragment foo = (([foo])[@JSX ])
--- ppx/test/test_src.re
+++ ppx/test/test_src.re
@@ -15,6 +15,45 @@ let make = (~name="") => {
   </>;
 };
 
+[@react.component]
+let make = (~a, ~b, _) => {
+  Js.log("This function should be named `Test`");
+  <div />;
+};
+
+/* Not supported yet, how does RR do it without matching against `Pexp_field`??? */
+/*
+ module Foo = {
+   [@react.component] [@bs.module "Foo"]
+   external component: (~a: int, ~b: string, _) => React.element = "";
+ };
+ <Foo.component a=1 b="1" />;
+ */
+
+module Bar = {
+  [@react.component]
+  let make = (~a, ~b, _) => {
+    Js.log("This function should be named `Test$Bar`");
+    <div />;
+  };
+  [@react.component]
+  let component = (~a, ~b, _) => {
+    Js.log("This function should be named `Test$Bar$component`");
+    <div />;
+  };
+};
+
+module type X_int = {let x: int;};
+
+module Func = (M: X_int) => {
+  let x = M.x + 1;
+  [@react.component]
+  let make = (~a, ~b, _) => {
+    Js.log("This function should be named `Test$Func`", M.x);
+    <div />;
+  };
+};
+
 module ForwardRef = {
   [@react.component]
   let make =
@@ -22,3 +61,5 @@ module ForwardRef = {
       <div ref=theRef> "ForwardRef"->React.string </div>
     );
 };
+
+let fragment = foo => <> foo </>;
