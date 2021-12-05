594e9e5ec2052ff634d494e9a6ad5302710f7934
David Sancho Moreno <dsnxmoreno@gmail.com>
2021-12-06 00:30:28 +0100

    style: Add a space on val def in mlis

--- ppx/html.mli
+++ ppx/html.mli
@@ -27,4 +27,5 @@ type prop = Attribute of attribute | Event of event
 type errors = [`ElementNotFound | `AttributeNoMatch]
 
 val getHtmlName : prop -> string
+
 val findByName : string -> string -> (prop, errors) result
