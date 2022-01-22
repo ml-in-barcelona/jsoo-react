6e8effa9e8ce53e51cccf464997b51110d490b74
glennsl <glenn@slotte.net>
2022-01-22 15:08:28 +0100

    add web_component example

--- example/src/HelloWorldOCaml.ml
+++ example/src/HelloWorldOCaml.ml
@@ -23,3 +23,30 @@ let%component make () =
               ];
           ];
       ])
+
+let web_component =
+  (* Partial example from https://github.com/seattletimes/component-leaflet-map *)
+  React.Dom.Html.(
+    h "leaflet-map"
+      Prop.[|string "lat" "47"; string "lng" "-122"; int "zoom" 5|]
+      [
+        h "tile-layer" Prop.[|string "layer" "stamen-toner"|] [];
+        h "map-marker"
+          Prop.[|string "lat" "23"; string "lng" "-80"; string "id" "findMe"|]
+          [ string "Popup text!" ];
+        h "geo-json"
+          Prop.[|string "src" "url.geojson"|]
+          [
+            h "geo-data" [||] [ string "OR GEOJSON DATA GOES HERE" ];
+            h "geo-palette"
+              Prop.[|string "property" "FEATURE_PROPERTY"|]
+              [
+                h "color-mapping"
+                  Prop.[|int "max" 128; string "color" "red"|]
+                  [];
+                h "color-mapping"
+                  Prop.[|int "min" 129; int "max" 255; string "color" "blue"|]
+                  [];
+              ];
+          ];
+      ])
