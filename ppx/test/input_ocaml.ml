c1f30f0d487648dc22089d0b35e9c6b3560de350
Javier Chávarri <javier.chavarri@gmail.com>
2022-01-30 20:20:41 +0100

    ppx: remove useless tests from ocaml suite

--- ppx/test/input_ocaml.ml
+++ ppx/test/input_ocaml.ml
@@ -1,21 +1,17 @@
 let%component make ?(name = "") = div [||] [React.string ("Hello " ^ name)]
 
-let element = div [|id "foo"|] []
+let%component make ~children:(first, second) = div [||] [first; second]
 
-let element = a [|id "foo"; href "https://www.hello.com"|] [React.string "hello"]
-
-let%component make ~children:(first, second) = div [|id "foo"|] [first; second]
-
-let%component make ~children:kids = div [|id "foo"|] kids
+let%component make ~children:kids = div [||] kids
 
 let%component make ~children:(first, second) () = div [||] [first; second]
 
 let%component make ?(name = "") = div [||] [name]
 
-let%component make () = a [|href "https://opam.ocaml.org"|] []
+let%component make () = div [||] []
 
 let%component make ~(bar : int option) =
-  div ~children:[React.string (string_of_int (Option.value ~default:0 bar))] ()
+  div [||] [React.string (string_of_int (Option.value ~default:0 bar))] ()
 
 external%component make : name:Js.js_string Js.t -> React.element
   = "require(\"my-react-library\").MyReactComponent"
