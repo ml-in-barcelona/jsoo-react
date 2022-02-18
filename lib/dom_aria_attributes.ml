61413de500a1d7bd4deedbed84dd32098f245cc5
David Sancho Moreno <dsnxmoreno@gmail.com>
2022-02-18 20:19:15 +0100

    refactor: Rename global_attr to aria_attr

--- lib/dom_aria_attributes.ml
+++ lib/dom_aria_attributes.ml
@@ -1,33 +1,5 @@
 include Dom_dsl_core.Prop
 
-let accessKey = string "accessKey"
-
-let className = string "className" (* substitute for "class" *)
-
-let contentEditable = bool "contentEditable"
-
-let contextMenu = string "contextMenu"
-
-let dir = string "dir" (* "ltr", "rtl" or "auto" *)
-
-let draggable = bool "draggable"
-
-let hidden = bool "hidden"
-
-let id = string "id"
-
-let lang = string "lang"
-
-let role = string "role" (* ARIA role *)
-
-let style = (any "style" : Dom.Style.t -> t)
-
-let spellCheck = bool "spellCheck"
-
-let tabIndex = int "tabIndex"
-
-let title = string "title"
-
 (* All the WAI-ARIA 1.1 attributes from https://www.w3.org/TR/wai-aria-1.1/ *)
 
 (* Identifies the currently active element when DOM focus is on a composite widget, textbox, group, or application. *)
