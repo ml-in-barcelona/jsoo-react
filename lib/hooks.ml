43c4834cf512c0a8056f65eab2ad3a4c588bd455
glennsl <glenn@slotte.net>
2022-03-27 13:27:22 +0200

    hooks: add aliases to non-deps hooks in Hooks

--- lib/hooks.ml
+++ lib/hooks.ml
@@ -6,6 +6,10 @@ let use_ref_lazy init =
   let state, _ = Core.use_state (fun () -> ref (init ())) in
   state
 
+let use_state initial = Core.use_state (fun () -> initial)
+let use_state_lazy init = Core.use_state init
+let use_reducer = Core.use_reducer
+
 let use_resource ~on:deps ?(equal = ( = )) ~release acquire =
   let last_deps = use_ref deps in
   let resource = use_ref None in
@@ -27,6 +31,16 @@ let use_resource ~on:deps ?(equal = ( = )) ~release acquire =
 let use_effect ~on ?equal ?(cleanup = fun () -> ()) f =
   use_resource ~on ?equal ~release:cleanup f
 
+let use_effect_once ?before_render ?cleanup f =
+  Core.use_effect_once ?before_render (fun () ->
+      f ();
+      cleanup)
+
+let use_effect_always ?before_render ?cleanup f =
+  Core.use_effect_always ?before_render (fun () ->
+      f ();
+      cleanup)
+
 let use_memo ~on:deps ?(equal = ( = )) f =
   let last_deps = use_ref deps in
   let value = use_ref_lazy (fun () -> f ()) in
@@ -37,3 +51,5 @@ let use_memo ~on:deps ?(equal = ( = )) f =
   end;
 
   !value
+
+let use_context = Core.use_context
